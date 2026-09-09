52b650b04e25d41ae2d92e41cf768063141f1f66
techsalaf <amudarash102@gmail.com>
2026-09-09 19:57:04 +0100

    Fix missing Builder i18n module

---
+++ Modules/Builder/resources/js/i18n.js
@@ -0,0 +1,17 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      translation: {},
+    },
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+})
+
+export default i18n
