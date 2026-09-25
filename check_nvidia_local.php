bad89ac0dc97e1f9bf50c6308c52edb99038d961
techsalaf <amudarash102@gmail.com>
2026-09-25 05:47:49 +0100

    chore: gitignore scratch/debug scripts, remove junk files from index

--- check_nvidia_local.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ch = curl_init('https://integrate.api.nvidia.com/v1/models');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer test'
-]);
-$res = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-echo $code . " -> " . $res . "\n";
--- check_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-use Illuminate\Support\Facades\Schema;
-
-echo json_encode(Schema::getColumnListing('whatsapp_conversations'));
